c3965cc7045b89044281d79fb1333cffdeb00248
Ashish Shrestha <axhixh@users.noreply.github.com>
2014-04-03 19:18:50 +0100

    considers time series has anomaly only if 60% of the algos agree.

--- src/main/java/gaul/psychoanalytix/algorithms/Analyzer.java
+++ src/main/java/gaul/psychoanalytix/algorithms/Analyzer.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
  */
 public class Analyzer {
     private static final Logger logger = LogManager.getLogger("psychoanalytix.analyzer");
-    private static final int MIN_NUM_POINTS = 12; // assuming most stats are 5 mins we want at least 1 hr
+    private static final int MIN_NUM_POINTS = Integer.getInteger("psycho.min_num_points",12); // assuming most stats are 5 mins we want at least 1 hr
     
     public void analyze(Collection<Metric> metrics) {
         for (Metric m : metrics) {
@@ -26,10 +26,16 @@ public class Analyzer {
         if (values.length < MIN_NUM_POINTS) {
             logger.info("Skipping {} because number of data points is less than minimum threshold", m.getName());
         }
-        for (Algorithm algo : AlgorithmList.getAll()) {
-            // do consensus instead of individual algo
+        final Algorithm[] algos = AlgorithmList.getAll();
+        for (Algorithm algo : algos) {
+            int count = 0;
             if (algo.hasAnomaly(m)) {
                 logger.info("{} has an anomaly based on {}", m.getName(), algo.getClass().getSimpleName());
+                count++;
+            }
+            
+            if (count > 0.6 * algos.length) {
+                logger.info("FOUND ANOMALY");
             }
         }
     }
